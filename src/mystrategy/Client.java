2ce144261329a14e3263a15cbd2eb7045669cc39
Justyna Konior <justyna28102@gmail.com>
2021-06-17 11:37:19 +0200

    Added answers to questions at the end of Client code

--- src/mystrategy/Client.java
+++ src/mystrategy/Client.java
@@ -26,7 +26,7 @@ public class Client {
         // http://www.algorytm.org/algorytmy-sortowania/
         
         //double[] dataNonSort = DataGenerator.generate(100000);
-        double[] dataNonSort = DataGenerator.generate(1000);
+        double[] dataNonSort = DataGenerator.generate(10000);
         System.out.println(Arrays.toString(dataNonSort));
         Choice sorting = new Choice();
        /* Wzorzec Stratega */
@@ -41,3 +41,14 @@ public class Client {
         System.out.println(Arrays.toString(dataNonSort));
     }
 }
+ //Odpowiedzi na pytania
+//1. Proszę o podanie głównego celu zastosowania wzorca strategii.
+//Wzorzec strategii wykorzystuje się w momencie, gdy istnieje potrzeba rozwiązania danego problemu na kilka różnych sposobów.
+//Zakłada przekształcenie zestawu zachowań w obiekty, które można stosować zamiennie w pierwotnym obiekcie.
+
+//2.  Czy istnieje możliwość zmiany algorytmu w czasie działania programu?
+// Strategia umożliwia wymienne stosowanie każdego z algorytmów w trakcie działania aplikacji niezależnie od korzystających z nich użytkowników.
+
+//3. W jaki sposób wzorzec enkapsuluje poszczególne algorytmy?
+//Poszczególne algorytmy tworzone są w oddzielnych klasach dziedziczących po klasie abstrakcyjnej. 
+//Ta klasa abstrakcyjna wykorzystywana jest poprzez wstrzykiwanie zależno
